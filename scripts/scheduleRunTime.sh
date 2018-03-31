1db7c46d308da2316c63f632e3fcab4ad2ac4c83
Mark Schwartz <mrbloozman@gmail.com>
2018-03-30 21:40:21 -0700

    enabled gpio and scripts folder

---
+++ scripts/scheduleRunTime.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/public/IrrigationControl
+python scheduleRunTime.py launch_params.json
---
+++ scripts/webservRunTime.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/public/IrrigationControl
+python webservRunTime.py launch_params.json
